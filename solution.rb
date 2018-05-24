b6733f906c08decaeda4d490a2a47ceccb2b49fd
sanra9 <sanrra999@gmail.com>
2018-05-23 20:33:25 -0500

    codigo secreto

--- solution.rb
+++ solution.rb
@@ -1,16 +1,20 @@
 require 'sinatra'
 
-get '/makers/:nombre' do
-  if params[:nombre] == nil || params[:nombre] == "" || params[:nombre] == []
-    "Hola desconocido!"
-  else
-    "Hola"+" #{params[:nombre].capitalize}!"
-  end
-end
 get '/' do
   erb :index
 end
 
-post '/saludo' do
-  "Hola #{params[:nombre]}!"
-end
+# get '/makers/:nombre' do
+#   if params[:nombre] == nil || params[:nombre] == "" || params[:nombre] == []
+#     "Hola desconocido!"
+#   else
+#     "Hola"+" #{params[:nombre].capitalize}!"
+#   end
+# end
+# get '/' do
+#   erb :index
+# end
+#
+# post '/saludo' do
+#   "Hola #{params[:nombre]}!"
+# end
